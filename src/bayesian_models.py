a5e7828d130c3a26dcab3074c4bdc21799bc8e3c
Arkaprava Chakraborty <arkapravac366@gmail.com>
2025-11-27 14:52:43 +0530

    Update bayesian_models.py , created the BayesianTImeSeriesModel class which initially initializing the  assets acc. to 4 chains per Vehtari et al. (2019)

--- src/bayesian_models.py
+++ src/bayesian_models.py
@@ -11,3 +11,14 @@ import config as cfg
 from src.utils.data import load_data, load_feature_columns
 from src.utils.metrics import calculate_bayesian_metrics
 from src.utils.viz import plot_bayesian_forecast
+
+class BayesianTimeSeriesModel:
+    def __init__(self, asset: str, n_samples: int = 1000, n_chains: int = 4):
+        self.asset = asset
+        self.n_samples = n_samples
+        self.n_chains = n_chains  
+        self.scaler = None
+        self.feature_cols = None
+        self.train_data = None
+        self.test_data = None
+        self.idata = None
